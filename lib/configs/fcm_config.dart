a1191f73a88beae5a490558834de46a682172f76
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-09-04 16:01:29 +0200

    first commit

---
+++ lib/configs/fcm_config.dart
@@ -0,0 +1,17 @@
+// Firebse Admin SDK for push notifications
+const Map<String, dynamic> serviceCreds = {
+  "type": "service_account",
+  "project_id": "seamens-club-ai-hub",
+  "private_key_id": "7cfb94c5f31ed34ba0157d568c978a9fedbb7166",
+  "private_key":
+      "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/9iPivLqALlwY\nkk5mDfNQ3M14U40k4zyicFpNMYUJkbniwUT8cvWVW96nEl3KbHRx/pniRPUYI/aS\nEtk1qc8sLXLOY4eub5OpETI0yzGs9J8cUWWzNveSBgegkVvGKfU2CwI/2JCcGHU4\n1CHYxgptdCoIj1ihW9LLPwLh7nJv9WCtVsJO59+SjOWAJesV6AnOdv/gTbCN3t1P\nIfk5hUolLJw2uDXYm7zuM9tcE2bbg+1q0fv3oGRy+yTX/mlnb5UO4yq6jDFLJ+vr\nADp2nMwEkLmSIkbsF0erGTxJYrWaSrzc68DRRthBvVrHZbx6lBgccb/Smwi78j+A\nhFujwSLXAgMBAAECggEACGDpcxGSwFqFqYrlQh4UwzBIBZr7+6KxeFOYFWt2Z5MF\nPoj6BlzMLE88uiUak+XE9V7L/6zL19hmAD0zXtzBDqgTLT/54Zq8IrJ98wD1c5uf\nwEArRLW++5Ao0BDYJR+atNepKvk8YpvMrgTQT2JhfSKHXSVIlI5p9nYTHaJnO0yy\npvx8OFtiuUzKsfC88qi9K92EFeEeg6MTNwKWtV9lLvfoITNQpnHPfBauy2pl9iCC\nF3YCIxRcrp1ElTBSisz4zu8KAEmM1reMe7P2kcWEA3IxEXICVo+j1otb7ehf2EhR\nelO6IYyVHtTZ3LEhM7J1HqPP2M/gB8WGRqrt3RGv/QKBgQDiQgTKJEvr25W1hKWU\nJUXZc1npJqRVeuonduaeoB7QUwDVpRL0a+7gbeIoHrLs7/rvAanJsddn9qdsLTcW\nrsGFXb8uHs1Z/plZJTZcfuJ7XaUHetFdRzUcfBR450BI9OkPw7dkfLDEfjyTieNU\njIXIXIGgz8hlGYXRcIf5KYVHQwKBgQDZMfyHMX2mmSmSzwdDWYyuOJi69ShE2zzE\nVc+JrU25u4VOdQ6PQwTXUc/Dm8bTGXLedsBULutUMVW3pEx4hZ8yBYxBCvAvprup\nKyVwzsa6tPP5AOQETqg61Cnn6wjOZ7R69+Tkmzkql7KngrCM0TlrcI/qVQx3i41+\nd7EpVc4K3QKBgFAmcuvoz7xNnUsgIkfIrGXLpx4gwjZFxI0WYbvvnPrPbIxCXeSY\nF5cJVgRAW8HzgUEJpcv9ZAFALy7zFYDDh2pnO0JrRvOKbWhW9lrt1+07zypHJ8OT\nZg0hJ19tDCjMGYOsIVtHkJcK4Qqi3hUlcn73xjPhZ2vpDq6S3pg1TTFhAoGABHUb\nKqcgXgfsq3+mNYKj+Xtd3xBi+C7Lq3fbX4b3/mTT0o1idBw7ADREoE4VIYX3Louf\nXNrtp3evu6oai6qqXrtwanFQBppUZZ9mEE+eEnzeOPEEvPXkS/jIt2LCdoH4WUG3\nJ93rqW0dlISBHRK6K/NOcM+4BsgNalgIgcGqDd0CgYEAhba/ZpsNzMcGM/mDgNDk\naBsp7ksZu6P1isQ/5LsWZv9E71Xd7hM6xjy7TXM+dKvAsjAxy5GybcqRE0qVz5IZ\npLZ/7SvM9k5Wnu/K6Q5hKceQ6sRl9PUG1PKi99XPVDvWV+lvzrlWdCfkgZJ3E//K\nohzsIFkfVpt1S9GAn5YWy/o=\n-----END PRIVATE KEY-----\n",
+  "client_email":
+      "firebase-adminsdk-sv3yr@seamens-club-ai-hub.iam.gserviceaccount.com",
+  "client_id": "113929554703273034985",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url":
+      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-sv3yr%40seamens-club-ai-hub.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+};
